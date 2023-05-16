8a0b2b43d3246074f70988fa5c4d695a794ef060
Ntomsana10 <thandontomsana@gmail.com>
2023-05-16 18:59:16 +0200

    Poll system with a single question

---
+++ Poll/polls/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.http import Http404
+from django.urls import reverse
+from .models import Question, Choice
+# Create your views here.
+# @DjangoAdmin10
+
+def index(request):
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    # template = loader.get_template("polls/index.html")
+    context = {
+        "latest_question_list": latest_question_list,
+    }
+
+    return render(request, "polls/index.html", context)
+
+def detail(request, question_id):
+
+    question = get_object_or_404(Question,pk=question_id)
+
+    return render(request, "polls/detail.html/", {"question": question})
+
+def results(request, question_id):
+    response = "You're looikng at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+
+    try:
+        selected_chioce = question.choice_set.get(pk=request.POST['choice'])
+
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, "polls/detail.html", {
+            "question": question, "error message":"You didn't select a choice"
+        },)
+    
+    else:
+        selected_chioce.votes += 1
+        selected_chioce.save()
+        return HttpResponseRedirect(reverse("polls:results", args=(question_id,)))
